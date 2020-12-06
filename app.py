5f2d20be2f095b5c16fbf0abee34c2b92a874ca1
Evgeniy <jacksonrr3@yandex.ru>
2020-12-06 22:57:32 +0300

    app update

--- app.py
+++ app.py
@@ -4,38 +4,38 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
-def render_main():
-    return 'Здесь будет главная'
+def render_index():
+    return render_template('index.html')
 
 @app.route('/all/')
 def render_all():
-    return 'здесь будут преподы'
+    return render_template('all.html')
 
 @app.route('/goals/<goal>/')
 def render_goal(goal):
-    return f'сдесь будет цель {goal}'
+    return render_template('goal.html')
 
-@app.route('/profiles/<int:id_teacher>/')
-def render_teacher(id_teacher):
-    return f'здесь будет препод {id_teacher}'
+@app.route('/profiles/<int:teacher_id>/')
+def render_teacher(teacher_id):
+    return render_template('profile.html', teacher_id=teacher_id)
 
 @app.route('/request/')
 def render_request():
-    return 'здесь будет заявка'
+    return render_template('request.html')
 
 @app.route('/request_done/')
 def route_request_done():
-    return 'заявка на подбор отправлена'
+    return render_template('request_done,html')
 
-@app.route('/booking/<int:id_teacher>/<day>/<time>/')
-def route_booking_form(id_teacher, day, time):
-    return f'сдеаь будет форма бронирования {id_teacher}'
+@app.route('/booking/<int:teacher_id>/<day>/<time>/')
+def route_booking(teacher_id, day, time):
+    return render_template('booking.html')
 
 @app.route('/booking_done/')
 def route_booking_done():
-    return 'заявка отправлена'
+    return render_template('booking_done.html')
 
-
+app.debag = true
 
 app.run('0.0.0.0', 8000)
 
